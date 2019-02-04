14da46de990dc1478627971b52e557aef451274e
Gessy90 <gessicasantoro90@hotmail.it>
2019-02-04 11:54:11 +0000

    Added PORT environment variable

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const express = require('express')
 
 const app = express()
@@ -5,4 +7,4 @@ const app = express()
 app.use(express.static(`${__dirname}/dist`))
 app.get('/*', (req,res) => res.sendFile(`${__dirname}/dist/index.html`))
 
-app.listen(4000, ()=> console.log('Express is serving the dist folder on port 4000'))
+app.listen(process.env.PORT, ()=> console.log(`Running on port ${process.env.PORT}`))
